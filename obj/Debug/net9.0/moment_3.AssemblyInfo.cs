4e8d68f3d7a8794a6d5d04a93c8a3b009022c2dc
Markus <markus.vickman@hotmail.com>
2025-02-12 14:37:07 +0100

    skapade datahantering i routes för boklån
    -Vid route "lägg till boklån" tas även en bok bort från saldot vald bok.
    
    -Denna kod kopierades sedan till delete routen för med kortades ner och anpassades för att istället lägga till en till saldot när en bok lämnas tillbaka.

--- obj/Debug/net9.0/moment_3.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9800b5e334a9dcd8aa21717ff3bf7737b84c7094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a399bab6898a3695b264c1f24d0843506bfe288d")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
